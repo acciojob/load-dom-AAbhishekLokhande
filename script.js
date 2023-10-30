f8740edf23ada30464b01484747d7143ddca203f
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-30 15:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded', function() {
-    let body = document.getElementsByTagName('div')[0];
+    let body = document.getElementsByTagName('body')[0];
     body.innerText = "DOM load success";
 });
