94805676a7ade19ccaab4d907888e84954e46e56
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-30 15:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    var body = document.getElementsByTagName('body')[0];
+    body.innerHTML = "DOM load success";
+});
